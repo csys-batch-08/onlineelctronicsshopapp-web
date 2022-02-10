17045b38ddd009042489fb37c6f0a4b8dce5ae5c
Soniya V <soniyarajam2911gmail.com>
2022-02-10 14:29:46 +0530

    updated servlet file

--- src/main/java/com/onlineelectronicshop/servlet/LoginServlet.java
+++ src/main/java/com/onlineelectronicshop/servlet/LoginServlet.java
@@ -27,7 +27,6 @@ public class LoginServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
